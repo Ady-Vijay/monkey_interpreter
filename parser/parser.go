c9e2163bf90e7d785adcc0dadcbadf9ae2bd1f74
Ady-Vijay <ady.vijay@gmail.com>
2025-06-25 20:06:55 -0500

    commiting finished parser test 2.4

---
+++ parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"interpreter/ast"
+	"interpreter/lexer"
+	"interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
